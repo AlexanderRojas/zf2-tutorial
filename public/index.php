f269fbde559ef6f2a049d3aa977154c26126ac17
Alexánder Rojas <rojjaas@gmail.com>
2018-01-13 19:43:40 -0500

    El problema se resolvio cambiando version PHP. Se estudió y comentó el index.php

--- public/index.php
+++ public/index.php
@@ -4,10 +4,36 @@ use Zend\Mvc\Application;
 use Zend\Stdlib\ArrayUtils;
 
 /**
+ * when using Apache. Si pongo la variable APPLICATION_ENV = "development"
+ * Entonces ponga a PHP  mostrar TODOS los errores.
+ * Display all errors when APPLICATION_ENV is development.
+ * you can use the APPLICATION_ENV setting in your VirtualHost to let PHP
+ * output all its errors to the browser. This can be useful during the development of your application.
+ */
+ if ($_SERVER['APPLICATION_ENV'] == 'development') {
+     error_reporting(E_ALL);
+     ini_set("display_errors", 1);
+     } // Ya la configuré en vhost Apache
+
+/**
  * This makes our life easier when dealing with paths. Everything is relative
  * to the application root now.
  */
+echo getcwd() . "\n";
 chdir(dirname(__DIR__));
+echo getcwd() . "\n";
+/**
+ * __DIR__ : Constante mágica (se devuelven durante la compilación y cambia según dónde se empleen)
+ * Directorio del fichero. Si se utiliza dentro de un include, devolverá el directorio del fichero incluido.
+ * Esta constante es igual que dirname(__FILE__). El nombre del directorio no lleva la barra finla a no ser que 
+ * esté en el directorio root.
+ * 
+ * dirname() : Develve la ruta de un directorio padre.
+ * chdir(dirx): cambia el directorio actual a dirx
+ * Veo que lo que hace acá es  deshacerse de /public
+ * pasa de C:\wamp\www\zf2-tutorial\public  a C:\wamp\www\zf2-tutorial 
+ */
+
 
 // Decline static file requests back to the PHP built-in webserver
 if (php_sapi_name() === 'cli-server') {
@@ -16,10 +42,11 @@ if (php_sapi_name() === 'cli-server') {
         return false;
     }
     unset($path);
-}
+}//Esto es para en caso de usar server integrado. No lo estudio por el momento
 
 // Composer autoloading
 include __DIR__ . '/../vendor/autoload.php';
+// Se suponde que carga todas las clases. Las deja disponibles en los archivos.
 
 if (! class_exists(Application::class)) {
     throw new RuntimeException(
